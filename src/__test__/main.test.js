e15a1342ef97b9ca949b087e321140a79fdef545
narip <tto9208@gmail.com>
2021-12-23 01:48:34 +0900

    feat: test inline

--- src/__test__/main.test.js
+++ src/__test__/main.test.js
@@ -6,14 +6,12 @@ describe('testMultiplication', () => {
     expect(five.amount).toBe(5)
   })
 
-  const product = five.times(2)
   test('$5 X 2 = 10', ()=>{
-    expect(product.amount).toBe(10)
+    expect(new Dollar(10)).toBe(five.times(2))
   } )
 
-  const product2 = five.times(3)
   test('$5 X 3 = 15', ()=>{
-    expect(product2.amount).toBe(15)
+    expect(new Dollar(15)).toBe(five.times(3))
   } )
 
   test('test equality success', () => {
